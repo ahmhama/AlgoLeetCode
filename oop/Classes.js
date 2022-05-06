6470e0aed3412d69db8ad0968bab500f4da7aaff
eman8111 <eman2laa99@gmail.com>
2022-05-06 22:32:46 +0200

    OOP

---
+++ oop/Classes.js
@@ -0,0 +1,43 @@
+/* -------------------------------------------------------------------------- */
+/*                                    Class                                   */
+/* -------------------------------------------------------------------------- */
+
+// class : is set of instructions to build specific type of object
+
+// to Define a class we need to following things
+// 1. class name
+// 2. Data members (variables)
+// 3. methods
+
+/* -------------------------------------------------------------------------- */
+/*                                   Syntax                                   */
+/* -------------------------------------------------------------------------- */
+
+class ClassName {
+    constructor() { // Always add this method
+        // constructor : is a function that is called when a new object is created
+        // it is used to initialize object properties
+        // if u have more than one constructor will return (SyntaxError)
+    }
+}
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  important                                 */
+/* -------------------------------------------------------------------------- */
+
+//One important aspect of classes is, unlike function declarations, classes are hoisted.
+//This means that you cannot create an object before accessing it, otherwise the code will throw a (ReferenceError).
+
+/* -------------------------------------------------------------------------- */
+/*                         to create object from class                        */
+/* -------------------------------------------------------------------------- */
+let nameObj = new ClassName(); //u must pass the class name
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                    super                                   */
+/* -------------------------------------------------------------------------- */
+// super : is used to access the parent class (inheritance)
